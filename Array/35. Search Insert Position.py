9cd1e59fed75a4588d29ad810bcb169194f53f7b
BigdataXiangGD <32273831+BigdataXiangGD@users.noreply.github.com>
2018-01-16 21:56:37 +0000

    Create 35. Search Insert Position.py

---
+++ Array/35. Search Insert Position.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (right - left)/2 + left
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
+        
+        #Time O（logn）; Space O(1)
+        #Binary Search
