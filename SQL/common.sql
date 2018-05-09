5c9a6a7b01a4433bdca50b9ef8ebc687a598a39b
BigdataXiangGD <32273831+BigdataXiangGD@users.noreply.github.com>
2018-05-09 17:05:59 +0100

    Update common.sql

--- SQL/common.sql
+++ SQL/common.sql
@@ -1,4 +1,4 @@
-/*1. return employee record with highest salary*/
+``````/*1. return employee record with highest salary*/
 select *
 from employee
 where salary = (select Max(salary) from employee)
@@ -10,3 +10,18 @@ select Max(salary) from employee
 select Max(salary) 
 from employee 
 where salary not in (select Max(salary) from employee)
+
+/*4. select range of employee based on id*/
+select *
+from employee
+where employee_id between 2003 and 2008
+
+
+
+
+
+
+
+
+
+
