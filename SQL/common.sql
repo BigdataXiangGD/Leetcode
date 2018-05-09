f9b1109d3b7b1500084274ca3bc5cf252b357c79
BigdataXiangGD <32273831+BigdataXiangGD@users.noreply.github.com>
2018-05-09 15:55:38 +0100

    Update common.sql

--- SQL/common.sql
+++ SQL/common.sql
@@ -2,3 +2,11 @@
 select *
 from employee
 where salary = (select Max(salary) from employee)
+
+/*2. select the highest salary in the employee table*/
+select Max(salary) from employee
+
+/*3. select second highest salary from employee*/
+select Max(salary) 
+from employee 
+where salary not in (select Max(salary) from employee)
