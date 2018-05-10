e1288fc416e2453a2d94f46b7c0599641ba198bb
BigdataXiangGD <32273831+BigdataXiangGD@users.noreply.github.com>
2018-05-10 12:24:45 +0100

    Update common.sql

--- SQL/common.sql
+++ SQL/common.sql
@@ -1,4 +1,7 @@
-``````/*1. return employee record with highest salary*/
+
+/*https://www.youtube.com/watch?v=uAWWhEA57bE*/
+
+/*1. return employee record with highest salary*/
 select *
 from employee
 where salary = (select Max(salary) from employee)
@@ -16,8 +19,11 @@ select *
 from employee
 where employee_id between 2003 and 2008
 
-
-
+/*5. return employee name, highest salary and department*/
+select name,salary,department 
+from employee 
+inner join department on employee.department_id = department.department_id
+where salary = (select Max(salary) from employee)
 
 
 
