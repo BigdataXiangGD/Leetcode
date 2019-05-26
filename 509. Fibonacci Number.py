056b56e9bbcd1ff47e7c01f745914d2f35b2652b
BigdataXiangGD <32273831+BigdataXiangGD@users.noreply.github.com>
2019-05-26 16:11:38 +0800

    Update 509. Fibonacci Number.py

--- 509. Fibonacci Number.py
+++ 509. Fibonacci Number.py
@@ -1,3 +1,5 @@
+
+#recursive
 class Solution:
     def fib(self, N):
 
@@ -7,3 +9,20 @@ class Solution:
             return 1
         else:
             return self.fib(N - 1) + self.fib(N - 2)
+
+        
+#iterative        
+class Solution:
+    def fib(self, N):
+
+        if N == 0:
+            return 0
+        elif N == 1:
+            return 1
+        pre1 = 1
+        pre2 = 0
+        for i in range(2, N+1):
+            res = pre1 + pre2
+            pre2 = pre1
+            pre1 = res
+        return res
