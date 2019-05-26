22b8e78ff5f655f5929d0c662f05646c29812af1
BigdataXiangGD <32273831+BigdataXiangGD@users.noreply.github.com>
2019-05-26 17:08:44 +0800

    Update 509. Fibonacci Number.c

--- Array/509. Fibonacci Number.c
+++ Array/509. Fibonacci Number.c
@@ -1,5 +1,5 @@
 
-
+#recursive
 int fib(int N){
     if(N == 0){
         return 0;
@@ -13,3 +13,23 @@ int fib(int N){
 
 }
 
+
+
+
+#iterative
+int fib(int N){
+    if(N == 0){
+        return 0;
+    }
+    else if(N == 1){
+        return 1;
+    }
+    int pre1 = 1;
+    int pre2 = 0;
+    int res = 0;
+    for(int i = 2; i <= N; i++){
+        res = pre1 + pre2;
+        pre2 = pre1;
+        pre1 = res;
+    }return res;
+}
